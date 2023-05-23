953614a378da4b8e56b277c3c982210a41653910
aditbest5 <aditbest5@gmail.com>
2023-05-23 13:21:17 +0700

    mysql

--- app/FaceRecognition.py
+++ app/FaceRecognition.py
@@ -2,7 +2,44 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import mysql.connector
+import time
+import datetime
 
+    
+def my_connection():
+        conn = mysql.connector.connect(
+                        host="sql12.freesqldatabase.com",
+                        user="sql12619087",
+                        password="8MTTpb3Hmt",
+                        database="sql12619087"
+                        )
+        return conn
+def getQuery(face_id):
+    conn = my_connection()
+    cursor = conn.cursor()
+    query = "SELECT NIM FROM akun where face_id = \"{face_id}\"".format(face_id=face_id)
+    cursor.execute(query)
+    hasil = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if hasil is not None:
+        nim = hasil[0]
+        return nim
+    else:
+        print("Tidak ada nim yang ditemukan dengan kondisi tertentu.")
+
+def query(name, face_id):
+    ts = time.time()
+    date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d');
+    timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+    conn = my_connection()
+    cursor = conn.cursor()
+    nim = getQuery(face_id)
+    query = "INSERT INTO image SET DATE = \"{date}\", student_name = \"{name}\", TIMEIN=\"{time}\", face_id =\"{face_id}\", NIM = \"{nim}\"".format(date=date,name=name,time=timeStamp,face_id=face_id,nim=nim)
+    cursor.execute(query)
+    conn.commit()
+    conn.close()
     
 def gen_frames():
     dataset_folder = "dataset/"
@@ -93,6 +130,8 @@ def gen_frames():
                     label_text = "%s (%.2f %%)" % (labels[idx], confidence)
                     name = labels[idx].replace("_", " ").split("-")[0]
                     face_id = labels[idx].split("-")[1]
+                    query(name, face_id)
+
        
                 else:
                     label_text = "%s (%.2f %%)" % ("unknown", confidence)
